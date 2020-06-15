13a7c77837bbe4719229ec2cf9ef817c591090e0
Dustin Knopoff <dustinknopoff@gmail.com>
2020-06-15 13:01:24 -0400

    Initial Commit

---
+++ is-dark-mode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  is-dark-mode
+//
+//  Created by Dustin Knopoff on 6/15/20.
+//  Copyright © 2020 Dustin Knopoff. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
